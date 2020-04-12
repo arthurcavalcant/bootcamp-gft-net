27d97334d88bc6fe125e993d80afb5783d8c0bd7
Arthur Silva <arthur.ferreira@ccc.ufcg.edu.br>
2020-04-12 02:59:25 -0300

    Initial commit

---
+++ Curso_NET_MVC/Curso_NET_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Curso_NET_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
